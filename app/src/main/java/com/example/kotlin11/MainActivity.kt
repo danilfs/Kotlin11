60e902456c1775926fe4e7d8c2aa6718d54702d3
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2022-02-21 13:25:18 +0300

    Data class,While, For

--- app/src/main/java/com/example/kotlin11/MainActivity.kt
+++ app/src/main/java/com/example/kotlin11/MainActivity.kt
@@ -1,3 +1,42 @@
 package com.example.kotlin11
 
-class MainActivity 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var click_button:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+            initView()
+    }
+    private fun initView(){
+        findViewById<Button>(R.id.click_button).setOnClickListener{
+            Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+        }
+    findViewById<Button>(R.id.data_class).setOnClickListener{
+        val spb = Weather(12, "Saint-Petersburg")
+        Toast.makeText(this, spb.toString(), Toast.LENGTH_SHORT).show()
+
+        val samara = spb.copy(city = "Samara")
+        println(samara)
+    }
+        findViewById<Button>(R.id.while_circle).setOnClickListener{
+            var i = 2
+            while (i<10){
+                println("While ${i++}")
+            }
+        }
+        findViewById<Button>(R.id.for_circle).setOnClickListener{
+            for (i in 10 downTo 1){
+                println("for $i")
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/kotlin11/Weather.kt
@@ -0,0 +1,5 @@
+package com.example.kotlin11
+
+
+data class Weather(val temperature : Int , val city : String) {
+}
